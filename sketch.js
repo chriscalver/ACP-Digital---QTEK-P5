6d3163f05e816aeb1487d7b58a7e942e89fa7ca9
chriscalver <130410868+chriscalver@users.noreply.github.com>
2023-12-10 21:32:40 -0500

    picture

--- sketch.js
+++ sketch.js
@@ -1,28 +1,28 @@
 let qteklogo;
-let xpos = 360;
+let xpos;
 const message = 'Wire Chart';
-const messageX = xpos;
+//const messageX = xpos;
 const messageY = 330;
 const message2 = 'Electrical Drawing';
-const message2X = xpos;
+//const message2X = xpos;
 const message2Y = 370;
 const message3 = 'Pictures';
-const message3X = xpos;
+//const message3X = xpos;
 const message3Y = 410;
 const message4 = 'Terminal Layout';
-const message4X = xpos;
+//const message4X = xpos;
 const message4Y = 450;
 const message5 = 'Cable Layout Chart';
-const message5X = xpos;
+//const message5X = xpos;
 const message5Y = 490;
 const message6 = 'Backplate Layout';
-const message6X = xpos;
+//const message6X = xpos;
 const message6Y = 530;
 
 const NUM_IMGS = 5,
-  imgs = [];
+imgs = [];
 let currentImg = 0;
-
+let counter = 0;
 
 let menu = true;
 let pictures = false;
@@ -31,52 +31,43 @@ function preload() {
     qteklogo = loadImage('pics/QTEK.gif');
     font = loadFont('oswald.ttf');
 
-    // for (let i = 0; i < NUM_IMGS; i++) {
-    //     imgs[i] = loadImage(`http://picsum.photos/640/360/?random?sig=${i}`);
-    //   }
-    let images = 5;
+    let images = 2;
     for (let i = 0; i < images; i++) {
         path = 'pics/' + str(i) + '.jpg' // create a path to the image
         loaded_image = loadImage(path)     // load the image from the path
         imgs.push(loaded_image)             // add the loaded path to ims
-      }
+    }
+   // console.log(imgs);
 
 }
-
-console.log(imgs);
 
 function setup() {
-    createCanvas(1000, 700);
+    createCanvas(1400, 900);
+    xpos = width/2 - 100;
    // setupButtons();
+   // console.log("pictures" + " " + pictures);
+   // console.log("menu " + " " + menu);
+   // console.log("width " + " " + width);
 }
-
 
 
 
 function draw() {
-    background('WHITE');
+    background('yellow');
 
-   
-   
-   
-   
-   
-   
-   
-   
-   
+
     if (menu) {
-        push();
+        //push();
         imageMode(CENTER);
-        image(qteklogo, 450, 130, qteklogo.width, qteklogo.height);
-        pop();
-       // image(imgs[currentImg], 0, 0);
+        image(qteklogo, width/2, 130, qteklogo.width, qteklogo.height);
+        //pop();
+        // image(imgs[currentImg], 0, 0);
         textFont(font);
         fill('grey');
-        line(360, 235, 625, 235);
+        line(xpos, 235, 725, 235);
         // textAlign(CENTER);
         textSize(26);
-        if (isMouseInsideText(message, messageX, messageY)) {
+        if (isMouseInsideText(message, xpos, messageY)) {
             cursor(HAND);
             fill(0, 200, 255);
             //stroke(0, 200, 255);
@@ -85,8 +76,8 @@ function draw() {
             fill('grey');
             //  stroke(255);
         }
-        text(message, messageX, messageY);
-        if (isMouseInsideText(message2, message2X, message2Y)) {
+        text(message, xpos, messageY);
+        if (isMouseInsideText(message2, xpos, message2Y)) {
             cursor(HAND);
             fill(0, 200, 255);
             //stroke(0, 200, 255);
@@ -95,8 +86,8 @@ function draw() {
             fill('grey');
             //  stroke(255);
         }
-        text(message2, message2X, message2Y);
-        if (isMouseInsideText(message3, message3X, message3Y)) {
+        text(message2, xpos, message2Y);
+        if (isMouseInsideText(message3, xpos, message3Y)) {
             cursor(HAND);
             fill(0, 200, 255);
             //stroke(0, 200, 255);
@@ -105,8 +96,8 @@ function draw() {
             fill('grey');
             //  stroke(255);
         }
-        text(message3, message3X, message3Y);
-        if (isMouseInsideText(message4, message4X, message4Y)) {
+        text(message3, xpos, message3Y);
+        if (isMouseInsideText(message4, xpos, message4Y)) {
             cursor(HAND);
             fill(0, 200, 255);
             //stroke(0, 200, 255);
@@ -115,8 +106,8 @@ function draw() {
             fill('grey');
             //  stroke(255);
         }
-        text(message4, message4X, message4Y);
-        if (isMouseInsideText(message5, message5X, message5Y)) {
+        text(message4, xpos, message4Y);
+        if (isMouseInsideText(message5, xpos, message5Y)) {
             cursor(HAND);
             fill(0, 200, 255);
             //stroke(0, 200, 255);
@@ -125,8 +116,8 @@ function draw() {
             fill('grey');
             //  stroke(255);
         }
-        text(message5, message5X, message5Y);
-        if (isMouseInsideText(message6, message6X, message6Y)) {
+        text(message5, xpos, message5Y);
+        if (isMouseInsideText(message6, xpos, message6Y)) {
             cursor(HAND);
             fill(0, 200, 255);
             //stroke(0, 200, 255);
@@ -135,10 +126,11 @@ function draw() {
             fill('grey');
             //  stroke(255);
         }
-        text(message6, message6X, message6Y);
-        textFont(font);
-        fill('grey');
-        textSize(14);
+        text(message6, xpos, message6Y);
+
+        // textFont(font);
+        // fill('grey');
+        // textSize(14);
         // text('ACP Digital Solutions', xpos, height - 30);
         textSize(32);
         fill(198, 0, 0);
@@ -147,68 +139,114 @@ function draw() {
         fill('black');
         text('SUPERBOMBER #23352', xpos, 257);
         //image(qteklogo, 0, 0, qteklogo.width /2,  qteklogo.height /2);
-        console.log(qteklogo.width);
+
     }
-}
 
 
 
-if(pictures){
 
-menu = false;
 
-push();
+    if (pictures) {
+
+        //menu = false;
+        //console.log(pictures)     
+        //setupButtons();
+        push();
         imageMode(CENTER);
-        image(qteklogo, 450, 130, qteklogo.width, qteklogo.height);
+        
+        image(qteklogo, width - 200, 55, qteklogo.width / 2, qteklogo.height / 2);
         pop();
 
+        fill('blue');
+        // line(360, 235, 625, 235);
+        // textAlign(CENTER);
+        textSize(16);
+        text("Back to Main", width - 270, 150);
+        image(imgs[currentImg], 0, 20, imgs[currentImg].width*2, imgs[currentImg].height*2);
+
+        if (counter < 1) {
+            setupButtons();
+        }
+        console.log("pisc" + width);
+        counter++;
+       // console.log(counter);
+        //  setupButtons();
+    }
+
+
+
+
+
+
 }
 
-// const setupButtons = _ => {
-//     previous = createButton('Prev');
-//     previous.position(400, 400);
-    
-//     previous.mouseClicked(_ => {
-//       if (currentImg > 0) currentImg--;
-//     });
-  
-//     next = createButton('Next');
-//     next.position(450, 400);
-    
-//     next.mouseClicked(_ => {
-//       if (currentImg < imgs.length - 1) currentImg++;
-//     });
-//   };
+// end of draw
+
+
+
+
+
+
+const setupButtons = _ => {
+    previous = createButton('Prev');
+    console.log("button" + width);
+    previous.position(300, 100);
+
+    if (menu) {
+        previous.position(-940, 100);
+    }
+    previous.mouseClicked(_ => {
+        if (currentImg > 0) currentImg--;
+    });
+
+    next = createButton('Next');
+    next.position(300, 200);
+
+    if (menu) {
+        next.position(-1020, 100);
+    }
+    next.mouseClicked(_ => {
+        if (currentImg < imgs.length - 1) currentImg++;
+    });
+
+};
+
 
 function mouseClicked() {
-    if (isMouseInsideText(message, messageX, messageY)) {
+    if (isMouseInsideText(message, xpos, messageY)) {
         //  menu = false;
         //window.open('http://www.chriscalver.com/', '_blank');
         // window.open('/pics/QTEK.gif', '_blank');
         window.open('/data/Wirechart.pdf', '_blank');
     }
 
-    if (isMouseInsideText(message2, message2X, message2Y)) {
+    if (isMouseInsideText(message2, xpos, message2Y)) {
         //  menu = false;
         //window.open('http://www.chriscalver.com/', '_blank');
         window.open('/data/Electrical.pdf', '_blank');
         // window.open('/data/Wirechart.gif', '_blank');
     }
 
-    if (isMouseInsideText(message3, message3X, message3Y)) {
-        //  menu = false;
+    if (isMouseInsideText(message3, xpos, message3Y)) {
+
+        pictures = true;
+
+        menu = false;
+        console.log("pictures" + " " + pictures);
+        console.log("menu " + " " + menu);
+
         //window.open('http://www.chriscalver.com/', '_blank');
-        window.open('/pics/0.jpg', '_blank');
+        //window.open('/pics/0.jpg', '_blank');
         // window.open('/data/Wirechart.gif', '_blank');
     }
 }
 
-function isMouseInsideText(message, messageX, messageY) {
+function isMouseInsideText(message, xpos, messageY) {
     const messageWidth = textWidth(message);
     const messageTop = messageY - textAscent();
     const messageBottom = messageY + textDescent();
 
-    return mouseX > messageX && mouseX < messageX + messageWidth &&
+    return mouseX > xpos && mouseX < xpos + messageWidth &&
         mouseY > messageTop && mouseY < messageBottom;
 }
 
